4e37efb311a862e5655a63039e18a2fc8da4ff3f
David Bernal <david.bernal@febelink.com>
2020-12-02 20:16:38 +0100

    Fixing internal errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'menu', pathMatch: 'full'},
+    {path: '', redirectTo: 'menu/todas', pathMatch: 'full'},
     {
         path: 'menu',
         loadChildren: () =>
