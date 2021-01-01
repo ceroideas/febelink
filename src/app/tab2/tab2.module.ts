5cf431a45ec505c9f4d0bfc50f67bd9806216963
abritopach <adrbrpa1988@gmail.com>
2021-01-01 13:34:10 +0000

    Updated tabs translation messages

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,16 +1,13 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+
 import { Tab2Page } from './tab2.page';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
   imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
+    SharedModule,
     Tab2PageRoutingModule
   ],
   declarations: [Tab2Page]
