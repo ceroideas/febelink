012726bba11dba3b8f0f6015d6a4d57a9b9b7f6f
abritopach <adrbrpa1988@gmail.com>
2021-02-01 22:31:27 +0000

    FIXED: Display user image in the split panel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,17 @@
 export const environment = {
   production: false,
 
+  /*
   baseWebUrl: 'https://api.febelink.com/',
   API_URL: 'https://api.febelink.com/api/',
   API_URL_AUTH: 'https://api.febelink.com/api/auth/',
   SOCKET_URL: 'https://api.febelink.com:3425',
+  */
 
-  /*
   baseWebUrl: "http://localhost/",
   API_URL: "http://localhost:8000/api/",
   API_URL_AUTH: "http://localhost:8000/api/auth/",
   SOCKET_URL: 'http://localhost:3425',
-  */
 
   WEB_CLIENT_ID:
     "470264817351-dj55lg6n7u6fllmkrj90cg9lnjfqrvcn.apps.googleusercontent.com",
