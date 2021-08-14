c39a778b425e4a3a728539c8c35feff8cfa978de
Miguel Martin <miguelmartin97@hotmail.com>
2021-08-14 15:00:44 +0200

    fixed app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,12 @@ import { UtilitiesService } from './services/utilities.service';
 import { ApiService } from './services/api.service';
 import { Deeplinks } from '@ionic-native/deeplinks/ngx';
 import { NavController } from '@ionic/angular';
-import { JsonPipe } from '@angular/common';
 import { TranslateConfigService } from './services/translate/translate-config.service';
 import { Storage } from '@ionic/storage';
 import { AuthenticationService } from './services/authentication/authentication.service';
 import { IUser } from './models/user.model';
 import { SuscribirsePage } from './pages/suscribirse/suscribirse.page';
 import { ISector, ISubSector } from './models/sector.model';
-import { CookieService } from 'ngx-cookie-service';
-import { first, take } from 'rxjs/operators';
-import { NotificationService } from './services/notification.service';
 
 @Component({
     selector: 'app-root',
@@ -65,10 +61,7 @@ export class AppComponent implements OnDestroy{
     private storage: Storage,
     private menu: MenuController,
     public authenticationService: AuthenticationService,
-    private modalCtrl: ModalController,
-    private cookSvc: CookieService,
-    private activatedRoute:ActivatedRoute,
-    private notificationSvc:NotificationService
+    private modalCtrl: ModalController
   ) {
     this.initializeApp();
   }
@@ -103,9 +96,8 @@ export class AppComponent implements OnDestroy{
         this.getUserInfo();
       }
     });
-    
-    this.isRecomendation();
-    this.notificationSvc.getUnreadNotificationsCount();
+    // this.loginImplicito();
+  }
 
   setupLanguage() {
     const currentLanguage = this.translateService.getDefaultLanguage();
