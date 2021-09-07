1f91c2f9f78691ccdd3b8c90e23e6dbfabbf2ed2
Miguel Martin <miguelmartin97@hotmail.com>
2021-09-07 19:18:40 +0200

    styled checkout

--- src/app/landing/pages/checkout/checkout.page.ts
+++ src/app/landing/pages/checkout/checkout.page.ts
@@ -17,6 +17,10 @@ import {
 export class CheckoutPage implements OnInit {
   elements: Elements;
   card: StripeElement;
+  error:string = undefined;
+
+  numTokens:number = 200;
+
   elementsOptions: ElementsOptions = {
     locale: 'es'
   };
@@ -30,32 +34,71 @@ export class CheckoutPage implements OnInit {
       name: ['', Validators.required]
     });
 
+    const elementStyles = {
+      base: {
+        color: '#32325D',
+        fontWeight: 500,
+        fontFamily: 'Source Code Pro, Consolas, Menlo, monospace',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+  
+        '::placeholder': {
+          color: '#CFD7DF',
+        },
+        ':-webkit-autofill': {
+          color: '#e39f48',
+        },
+      },
+      invalid: {
+        color: '#E25950',
+  
+        '::placeholder': {
+          color: '#FFCCA5',
+        },
+      },
+    };
+
     this.stripeSvc.elements(this.elementsOptions).subscribe(elements => {
       this.elements = elements;
       if (!this.card) {
-        this.card = this.elements.create('card', {
-          style: {
-            base: {
-              iconColor: '#666ee8',
-              color: '#31325f',
-              lineHeight: '40px',
-              fontWeight: 300,
-              fontSize: '20px'
-            }
-          }
+        this.card = elements.create('cardNumber', {
+          style: elementStyles,
         });
-        this.card.mount('#card-element');
+        this.card.mount('#card-number');
+      
+        var cardExpiry = elements.create('cardExpiry', {
+          style: elementStyles,
+        });
+        cardExpiry.mount('#card-expiry');
+      
+        var cardCvc = elements.create('cardCvc', {
+          style: elementStyles,
+        });
+        cardCvc.mount('#card-cvc');
+        this.card.on('change', this.onCardChange)
       }
     });
   }
 
+  onCardChange = ({error}) => {
+    // debugger
+    if(error){
+      this.error = error?.message;
+      console.log('onCardChange', error)
+    } else {
+      this.error = null;
+    }
+  }
+
   buy() {
     const name = this.stripeTest.get('name').value;
+    debugger;
     this.stripeSvc.createToken(this.card, { name }).subscribe(result => {
       if (result.token) {
         console.log('Token', result.token);
       } else if (result.error) {
         console.log('Error', result.error.message);
+        this.error = result.error.message;
       }
     });
   }
