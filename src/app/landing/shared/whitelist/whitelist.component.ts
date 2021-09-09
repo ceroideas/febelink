944a08a5b320f1282836abc56196851c75f6f867
Miguel Martin <miguelmartin97@hotmail.com>
2021-09-09 17:18:00 +0200

    Removed payment and modified addEmailToWhitelist

--- src/app/landing/shared/whitelist/whitelist.component.ts
+++ src/app/landing/shared/whitelist/whitelist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
+import { UtilitiesService } from 'src/app/services/utilities.service';
 
 @Component({
   selector: 'app-whitelist',
@@ -12,16 +13,29 @@ export class WhitelistComponent {
 
   constructor(
     private api: ApiService,
+    private utils: UtilitiesService
   ) { }
 
   response:any
 
-  async addEmailToWhitelist(email: any) {
+  async addEmailToWhitelist(emailField: any) {
     // debugger
-    const formData = new FormData();
-    formData.append('email', email.value);
-    const responseObs:Observable<any> = await this.api._createData('emailWhitelist', formData);
-    this.response = await responseObs.pipe(first()).toPromise();
+    const email = emailField?.value;
+    if(!email) {
+      alert('Introduce tu email');
+      return;
+    }
+    await this.utils.showLoading();
+    try{
+      const formData = new FormData();
+      formData.append('email', email);
+      const responseObs:Observable<any> = await this.api._createData('emailWhitelist', formData);
+      this.response = await responseObs.pipe(first()).toPromise();
+    }
+    catch(error) {}
+    finally {
+      this.utils.dismissLoading();
+    }
     // console.log(this.response);    
   }
 
