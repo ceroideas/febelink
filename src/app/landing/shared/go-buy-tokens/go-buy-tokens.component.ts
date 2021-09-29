78d3b4e3aed0909ee1f908600a9706edf783a8d5
Abdias Natanael Vrech <abdias.work@gmail.com>
2021-09-29 15:06:53 +0200

    Le añadí la internacionalizacion al componente Go Buy Tokens

--- src/app/landing/shared/go-buy-tokens/go-buy-tokens.component.ts
+++ src/app/landing/shared/go-buy-tokens/go-buy-tokens.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
 
 @Component({
   selector: 'app-go-buy-tokens',
@@ -10,7 +11,7 @@ export class GoBuyTokensComponent {
 
   @Input() hideTitle: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor( private router: Router,  private translateService: TranslateConfigService ) { }
 
   goBuyTokens(){
     this.router.navigate(['token','buy'])
