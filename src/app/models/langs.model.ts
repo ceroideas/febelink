c2c179fe38489d534c1411c9c70b71b8775dae20
Abdias Natanael Vrech <abdias.work@gmail.com>
2021-09-29 16:01:53 +0200

    Agregado de clase abstracta para evitar la redeclaracion de idiomas

--- src/app/models/langs.model.ts
+++ src/app/models/langs.model.ts
@@ -1,14 +1,32 @@
 /**
  * Description [Interface to define User Language Selection.]
  *
- * @author abrito
+ * @author abdias
  * @version 0.0.1
  *
  * @interface
  */
 export interface ILang {
   id: number;
+  language: string;
   lang: string;
+  country: string,
   flag: string;
   shortCode: string;
+}
+
+/**
+ * Default Languages
+ */
+ export abstract class ILangDEFAULTS {
+    static spSP: ILang = { id: 0, language: 'Español', lang: 'es', country: 'es', flag: 'Flag_SP', shortCode: 'ES-ES' };
+    static enUK: ILang = { id: 1, language: 'English', lang: 'en', country: 'uk', flag: 'Flag_UK', shortCode: 'EN-UK' };
+
+    static getLangs() : Array<ILang> {
+      let arr: Array<ILang> = [
+        ILangDEFAULTS.spSP,
+        ILangDEFAULTS.enUK
+      ];
+      return arr;
+    }
 }
