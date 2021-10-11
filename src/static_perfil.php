14d57c320ae0fe0f0ccd654314ab64937b8e17ef
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-10-11 15:05:47 +0200

    Añadido de Script Twitter al Body

--- src/static_perfil.php
+++ src/static_perfil.php
@@ -1,6 +1,6 @@
 <?php
 // 1. get the content Id (here: an Integer) and sanitize it properly
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);;
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
 $default_img = filter_input(INPUT_GET, 'default_img', FILTER_SANITIZE_NUMBER_INT);
 
 // 2. get the content from a flat file (or API, or Database, or ...)
@@ -28,6 +28,18 @@ function makePage($data,$default_img) {
             $image = "https://api.febelink.com/storage/users/default.png";
 	}   
     }
+
+    // 0. Add Twitter Conversion Tracking
+    $twitter = "<!-- Twitter universal website tag code -->
+        <script>
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            // Insert Twitter Pixel ID and Standard Event data below
+            twq( 'init', 'o6r52' );
+            twq( 'track','PageView');
+        </script>
+        <!-- End Twitter universal website tag code -->";
    
     // 1. get the page
 	// 2. generate the HTML with open graph tags
@@ -43,7 +55,7 @@ function makePage($data,$default_img) {
     $html .= '<meta property="og:type" content="article"/>'.PHP_EOL;
     $html .= '<meta property="fb:app_id" content="895023747604792" />'.PHP_EOL;
     $html .= '</head>'.PHP_EOL;
-    $html .= '<body>'.$size.' - picture to display '.$image.' Name '.$name.' Description '.$description.'</body>'.PHP_EOL;
+    $html .= '<body>'.$size.' - picture to display '.$image.' Name '.$name.' Description '.$description. /* $twitter. */ '</body>'.PHP_EOL;
     $html .= '</html>';
     // 3. return the page
     echo $html;
