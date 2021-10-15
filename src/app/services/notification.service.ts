4d794b180dcc494843517643a6e3a573a1f6f0d5
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-10-15 09:53:28 +0200

    Actualizacion de Metodo

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -71,14 +71,10 @@ export class NotificationService {
   }
 
   titleNotification(
-    notifCount: UnreadNotificationsCount,
+    notifCount: number,
     totalUnreadMessages: number
   ) {
-    const count: number =
-      notifCount?.chats +
-      notifCount?.offers +
-      notifCount?.ratings +
-      totalUnreadMessages;
+    const count:number = notifCount + totalUnreadMessages;
 
     // Lo traigo al titulo que tiene asignado para a este añadirle el contador de notif
     const title: string = this.utils.getWebTitle();
