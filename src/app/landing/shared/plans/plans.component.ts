6c60b86d08739c7885dd5d6f1973381f493eb7a3
David Bernal <david.bernal@febelink.com>
2021-10-21 22:46:17 +0200

    Updating the team members.

--- src/app/landing/shared/plans/plans.component.ts
+++ src/app/landing/shared/plans/plans.component.ts
@@ -6,23 +6,42 @@ import { pricingData } from './data';
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
-  styleUrls: ['./plans.component.scss']
+  styleUrls: ['./plans.component.scss'],
 })
 
 /**
  * Plans component
  */
 export class PlansComponent implements OnInit {
-
   pricingData: Pricing[];
   teamMembers: TeamMember[] = [
-    { pic: 'JD', name: 'Juan E. Domínguez Portillo', tag: 'juan', linkedin: 'juan-e-dom%C3%ADnguez-portillo-305b72b4', liTag: 'es' },
-    { pic: 'DB', name: 'David Bernal Guerrero', tag: 'david', linkedin: 'david-bernal-guerrero-74a5ba10a/' },
-    { pic: 'MD', name: 'Miguel Díaz de Terán', tag: 'miguel', linkedin: 'migueldiazdeteran/' },
-    { pic: 'OM', name: 'Óscar Melchor Galán', tag: 'oscar', linkedin: '%C3%B3scar-melchor-gal%C3%A1n-b485a79/' }
+    {
+      pic: 'JD',
+      name: 'Juan E. Domínguez Portillo',
+      tag: 'juan',
+      linkedin: 'juan-e-dom%C3%ADnguez-portillo-305b72b4',
+      liTag: 'es',
+    },
+    {
+      pic: 'DB',
+      name: 'David Bernal Guerrero',
+      tag: 'david',
+      linkedin: 'david-bernal-guerrero-74a5ba10a/',
+    },
+    {
+      pic: 'MD',
+      name: 'Miguel Díaz de Terán',
+      tag: 'miguel',
+      linkedin: 'migueldiazdeteran/',
+    },
+    {
+      pic: 'MA',
+      name: 'Miguel Ángel Romero de los Llanos',
+      tag: 'miguelAngel',
+      linkedin: 'miguel-angel-romero-de-los-llanos/',
+    },
   ];
-  constructor() { }
-
+  constructor() {}
 
   ngOnInit(): void {
     // fetches the data
