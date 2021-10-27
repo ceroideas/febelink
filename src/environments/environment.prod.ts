5cc746e391440b4c0e3a629860ca0971a4a766b0
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-10-27 15:33:49 +0200

    Declaring Store Links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,4 +11,7 @@ export const environment = {
   stripe_publick_key:'pk_live_26EQ8gc0INPEdZjy6Iy8DOnK004mMtUILK',
   // GooglePlaces API
   G_PLACES_API_KEY: 'AIzaSyB-s7c6IF_G7k30a0kxWFgM21PDy_dSSCs',
+  // Store Urls
+  GOOGLE_STORE: 'https://play.google.com/store/apps/details?id=com.xerintel.febelink54831',
+  APP_STORE: 'https://febelink.us19.list-manage.com/track/click?u=2c62737fce42bba9ebb2f5d05&id=f9ee5da0af&e=45535eea63'
 };
