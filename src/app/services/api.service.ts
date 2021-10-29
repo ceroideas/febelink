95a596c7071e356615317ee090b2edc2f9c12010
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-10-29 10:26:47 +0200

    Sending link_url to api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -483,6 +483,7 @@ export class ApiService {
     sector,
     sub_sector,
     dni,
+    link_url,
     imagen,
     pass
   ) {
@@ -502,6 +503,7 @@ export class ApiService {
     formData.append('sector', sector);
     formData.append('sub_sector', sub_sector);
     formData.append('dni', dni);
+    formData.append('link_url', link_url);
     if (imagen !== undefined) formData.append('file', imagen);
     formData.append('email', email);
     formData.append('password', pass);
@@ -537,6 +539,7 @@ export class ApiService {
     sector,
     sub_sector,
     dni,
+    link_url,
     imagen
   ) {
     const formData = new FormData();
@@ -555,6 +558,7 @@ export class ApiService {
     formData.append('sector', sector);
     formData.append('sub_sector', sub_sector);
     formData.append('dni', dni);
+    formData.append('link_url', link_url);
     if (imagen !== undefined) formData.append('file', imagen);
     formData.append('email', email);
     return this._createData('editar-ofertante', formData);
