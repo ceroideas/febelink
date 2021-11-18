766f341fca53b0f602b28f06aecba0d2ac8d1ff4
Miguel Martin <miguelmartin97@hotmail.com>
2021-11-18 18:00:47 +0100

    Allow buying tokens with decimals

--- src/app/landing/shared/buy-tokens/buy-tokens.component.ts
+++ src/app/landing/shared/buy-tokens/buy-tokens.component.ts
@@ -45,8 +45,8 @@ export class BuyTokensComponent implements OnInit {
 
   async buyTokens(profile?: any){
     
-    const numTokens = this.numTokens || this.landingSvc.getNumTokens();
-    if(!numTokens) {
+    const numFiat = this.numFiat || this.landingSvc.getNumFiat();
+    if(!numFiat) {
       this.utils.showToast("Indica cuantos tokens quieres comprar");  
       return
     }
@@ -107,7 +107,7 @@ export class BuyTokensComponent implements OnInit {
         formData.append('place_id', userLanding.place_id);
 
         formData.append('telefono', userLanding.phone);
-        formData.append('num_tokens', numTokens.toString());
+        formData.append('tokens_cost_euros', numFiat.toString());
         formData.append('phase_tokens', phaseTokens.toString());
         formData.append('lang', lang);
 
@@ -141,7 +141,7 @@ export class BuyTokensComponent implements OnInit {
       modal.present();
     } else{
       this.landingSvc.setJustLogged(true);
-      this.landingSvc.setNumTokens(numTokens);
+      this.landingSvc.setNumFiat(numFiat);
       this.router.navigate(['login', 'token'])
     }
   }
@@ -168,18 +168,26 @@ export class BuyTokensComponent implements OnInit {
     this.landingSvc.setPhaseTokens(+event.detail.value)
     if(this.lastInput === LastInput.Fiat){
       this.fiatToTokens({target:{value: this.numFiat}});
-    } else if(this.lastInput === LastInput.Token){
-      this.tokensToFiat({target:{value: this.numTokens}});
-    }
+    } 
+    // else if(this.lastInput === LastInput.Token){
+    //   this.tokensToFiat({target:{value: this.numTokens}});
+    // }
   }
 
-  tokensToFiat(event){
-    this.numFiat = Math.round(event.target.value * this.tokenCost() * 100) / 100;
-    this.lastInput = LastInput.Token;
-  }
+  // tokensToFiat(event){
+  //   console.log("tokensToFiat");    
+  //   let tokens = event.target.value;
+  //   tokens = Math.round(tokens * 100) / 100
+  //   this.numTokens = tokens;
+  //   this.numFiat = Math.round(tokens * this.tokenCost() * 100) / 100;
+  //   this.lastInput = LastInput.Token;
+  // }
 
-  fiatToTokens(event){
-    this.numTokens = Math.trunc(event.target.value / this.tokenCost());
+  fiatToTokens(event){   
+    let fiat = event.target.value;
+    fiat = Math.round(fiat * 100) / 100
+    this.numFiat = fiat;
+    this.numTokens = Math.trunc(fiat / this.tokenCost() * 100) / 100;
     this.lastInput = LastInput.Fiat;
   }
 
