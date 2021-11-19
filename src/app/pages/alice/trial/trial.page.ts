fff9d4ed722a67a37e4110d8d5d281994e513aed
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-11-19 14:18:54 +0100

    Adapting Visual

--- src/app/pages/alice/trial/trial.page.ts
+++ src/app/pages/alice/trial/trial.page.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import * as aliceonboarding from 'aliceonboarding';
 import { Onboarding, OnboardingConfig, DocumentType } from "aliceonboarding";
 import "aliceonboarding/dist/aliceonboarding.css";
 import { ILangDEFAULTS } from 'src/app/models/langs.model';
+import { IUser } from 'src/app/models/user.model';
+import { KYCAliceService } from 'src/app/services/kyc.alice.service';
 import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
 import { AliceKYC } from './alice.model';
@@ -15,9 +18,9 @@ import { AliceKYC } from './alice.model';
 })
 export class TrialPage implements OnInit {
 
+  currentUser: IUser;
   email: string = '';
-  name: string = '';
-  surname: string = '';
+  
   creating: boolean = false;
   done: boolean = false;
 
@@ -30,42 +33,123 @@ export class TrialPage implements OnInit {
   // Selfie
   selfie: boolean = false;
 
-  welcomeButton;
-
-  aliceKYC: AliceKYC;
+  userToken: string;
 
   SANDBOX_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJpc3N1ZXItc2FuZGJveCIsInR5cCI6IlNBTkRCT1giLCJleHAiOjE2NDE1NjQyNDMsImlhdCI6MTYzNjM4MDI0MywiY2xpIjoiZmViZWxpbmstdHJpYWwifQ.LRnJX4GWcqKy-DWgLte6_4p8loIbpNFPJPf36gZNT5bYZVost3iKzbXH-7-WDiwVlPlVdnQ55pgQf0hFeLLJ3U03XwlqYKiaf1q0iwRetEpeM1V1jm3E1HOZ_-1A2i5MfxRpy0mJ2j6wy_omOPgZRe5FV23xsZW6yba9CKAfntNdaAf0ETJoP-0tfFcEGEfpVdpIsBv_rUCmjh9PADEY1UCgmsGQbnMm7L1wgT-LL9jqUhlwXB2894N8C0ubG7s-EB5ve9dbcQhXVN1xdoBnklMONSmk74NnRvrA7qqKk8jecZT26InIJI8QQyKcY7hd6PrpFKeukfYZSD3t5XG0sA";
 
   constructor(
       private utilities: UtilitiesService
+    , private kycAliceService: KYCAliceService
     , private translateService: TranslateConfigService
     , private router: Router
   ) { }
 
-  ngOnInit() { }
-
-  ionViewDidEnter() {
-    this.enableWelcomeButton();
+  async ngOnInit() {
+    this.currentUser = { ...(await this.utilities.getUserData()) };
   }
 
-  enableWelcomeButton() {
-    // Enable Welcome Button
-    this.welcomeButton = document.getElementById( 'welcomeButton' );
+  
+  //SEARCH COMPONENT
+  searchText: string = '';
+  keyText: string = '';
+  keywords: any = {};
+  keys: any = [];
+  openKeys: boolean = false;
+  selectorEnabled: boolean = false;
+  showCookies = false;
+  refreshTab: any;
+  
+  publishSearchForm: FormGroup;
 
-    console.log( 'welcomeButton: ', this.welcomeButton );
+  showCard: boolean = false;
 
-    if( this.welcomeButton )
-      this.welcomeButton.disabled = false;
+  //NEW SEARCH COMPONENT
+  addFocus() {
+    this.selectorEnabled = true;
   }
+
+  async search() {
+    console.log('SEARCH', this.searchText, this.selectorEnabled);
+    if (this.searchText === '') {
+      this.keys = [];
+      this.selectorEnabled = true;
+    }
+    this.publishSearchForm.patchValue({nombre: this.searchText});
+    /* if ((this.searchText.length > 2) && (this.selectorEnabled)) {
+      (await this.api.searchByKeys(this.searchText)).subscribe((keywords) => {
+        if (keywords.length !== 0) {
+          let keys = [];
+          for (let key of keywords) {
+            let item = { name: this.highlight(key.keyword), value: key.keyword };
+            keys.push(item);
+          }
+          this.keys = keys;
+        }
+        else {
+          setTimeout(() => {
+            this.selectorEnabled = false;
+            this.showCard = true;
+          }, 500);
+        }
+        console.log('keys', this.keys);
+      });
+    } */
+  }
+
+  removeFocus() {
+    setTimeout(() => {
+      this.keyText = this.searchText;
+      this.selectorEnabled = false;
+      this.keys.length = 0;
+    }, 500);
+  }
+
+  clearBtn() {
+    this.keywords = {};
+    this.keys = [];
+  }
+
+  detectKeyPressed(event) {
+    if ((event.key === 'Enter') && (this.searchText.length > 2)) {
+      this.showCard = true;
+      setTimeout(() => {
+        this.keys.length = 0;
+      }, 500);
+    }
+  }
+
+  async countrySelected(key) {
+    /* console.log('getSectorsByKeys');
+    this.keys = [];
+
+    (await this.api.getSectorsByKeys(key.value)).subscribe((keywords) => {
+      console.log('keywords', keywords);
+      this.searchText = key.value;
+      this.keyText = this.searchText;
+      this.keywords = keywords;
+      this.selectorEnabled = true;
+      this.publishSearchForm.patchValue({sector: this.keywords.main.sector_id});
+
+      this.subSectors = [];
+      this.loadSubSectors(this.keywords.main.sector_id);
+
+      this.showCard = true;
+      this.removeFocus();
+    }); */
+  }
+
+
+
+
+
+
+
+
+
+
 
   onEmail( email ) {
     this.email = email;
-  }
-  onName( name ) {
-    this.name = name;
-  }
-  onSurname( surname ) {
-    this.surname = surname;
   }
   onChecked( isChecked: boolean, type: string ) {
     switch( type ) {
@@ -93,18 +177,11 @@ export class TrialPage implements OnInit {
     }
 
     this.creating = true;
-    this.onUserInfo( this.setUserInfo( this.email, this.name, this.surname ));
+    this.onUserInfo( this.setUserInfo( this.email, this.currentUser.name ));
   }
   restart() {
     this.creating = false;
     this.done = false;
-  }
-
-  onData( data: string ) {
-    data =  JSON.parse( data );
-    this.aliceKYC = data as AliceKYC;
-    this.done = true;
-    console.log( 'alice: ', this.aliceKYC);
   }
 
   setUserInfo( email: string, firstName?: string, lastName?: string ) {
@@ -119,9 +196,9 @@ export class TrialPage implements OnInit {
     let authenticator = new aliceonboarding.SandboxAuthenticator( this.SANDBOX_TOKEN, userInfo );
     authenticator.execute()
       .then(userToken => {
-        console.log( 'userToken: ', userToken );
-        // this.aliceOnboardingWelcome( userInfo, userToken );
-        this.aliceOnboarding( userToken );
+        this.userToken = userToken;
+        this.aliceOnboardingWelcome( userInfo, userToken );
+        // this.aliceOnboarding( userToken );
       })
       .catch(error => {
         alert("Please, add a valid SANDBOX_TOKEN (JavaScript)\n" + error.toString());
@@ -134,8 +211,6 @@ export class TrialPage implements OnInit {
     new aliceonboarding.OnboardingWelcome( "alice-onboarding-mount", userInfo )
       .run(
         ( res ) => {
-          this.enableWelcomeButton();
-
           this.aliceOnboarding( userToken );
         },
         () => { this.onCancel(  ); }
@@ -148,7 +223,7 @@ export class TrialPage implements OnInit {
   
     new aliceonboarding.Onboarding( "alice-onboarding-mount", config )
       .run(
-        ( userInfo ) => { this.onSuccess( userInfo ); },
+        ( userInfo ) => { this.onFinished( userInfo ); },
         ( err ) => { this.onError( err ); },
         () => { this.onCancel(  ); }
       );
@@ -158,12 +233,12 @@ export class TrialPage implements OnInit {
     const lang: string = ILangDEFAULTS.getCurrentLang( this.translateService ).lang;
 
     let documentStageConfig = new aliceonboarding.DocumentStageConfig(
-      aliceonboarding.DocumentCapturerType.CAMERA, true, aliceonboarding.CameraType.BACK
+      aliceonboarding.DocumentCapturerType.ALL, true, aliceonboarding.CameraType.BACK
     );
 
     const config = new aliceonboarding.OnboardingConfig()
       // Load Document By FILE EXPLORER || CAMERA
-      .withAddDocumentStage( aliceonboarding.DocumentCapturerType.CAMERA )
+      .withAddDocumentStage( aliceonboarding.DocumentCapturerType.ALL )
 
       // Language
       .withCustomLocalization( lang );
@@ -187,14 +262,28 @@ export class TrialPage implements OnInit {
     return config;
   }
 
-  onSuccess( res ) {
+  async onFinished( res ) {
     console.log("Onboarding complete. User info: " + JSON.stringify( res ));
-    this.onData( JSON.stringify( res ));
+    
+    (await this.kycAliceService.validateUser( res.user_id )).subscribe(
+      ( response ) => {
+        this.kycAliceService.handleBackendToken( response );
+
+        console.log( 'response: ', response );
+
+        this.utilities.dismissLoading();
+      },
+      ( err ) => {
+        this.onError( err );
+      }
+    )
   }
 
   onError( err ) {
     console.error("Onboarding error. Error: " + err.toString());
     this.done = false;
+
+    this.utilities.dismissLoading();
   }
 
   onCancel(  ) {
