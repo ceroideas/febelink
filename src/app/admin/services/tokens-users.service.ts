5342b6b1406de299f77b012ba1682300ace5c971
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-13 20:33:27 +0100

    Search User and Create Methods

--- src/app/admin/services/tokens-users.service.ts
+++ src/app/admin/services/tokens-users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { UtilitiesService } from 'src/app/services/utilities.service';
 import { ApiService } from '../../services/api.service';
 import { TokensUser } from '../models/tokens-user';
 
@@ -8,7 +9,8 @@ import { TokensUser } from '../models/tokens-user';
 export class TokensUsersService {
 
   constructor(
-    private api: ApiService
+      private api: ApiService
+    , private utilities: UtilitiesService
   ) { }
 
   async getTokensUsers(filter?:string){
@@ -17,19 +19,34 @@ export class TokensUsersService {
     return (await this.api._createData('admin/getTokensUsers',formData)).toPromise();
   }
 
-  async deleteTokensUser(id:number){
+  async getUsersByKey( keys?:string ) {
+    const formData = new FormData();
+    if( keys ) formData.append('keys', keys );
+    return ( await this.api._createData( 'admin/getUsersByKey',formData )).toPromise();
+  }
+
+  async createTokenUser( tokenUser: TokensUser ){
+    const formData = new FormData();
+    formData.append( 'uid', tokenUser.id.toString() );
+    formData.append( 'num_tokens', tokenUser.num_tokens.toString() );
+    formData.append( 'phase_tokens', tokenUser.phase_tokens.toString() );
+    formData.append( 'payed_date', tokenUser?.payed_date?.toString() || '' );
+    return ( await this.api._createData( 'admin/createTokenUser',formData )).toPromise();
+  }
+
+  async deleteTokenUser(id:number){
     const formData = new FormData();
     formData.append('id', id.toString());
-    return (await this.api._createData('admin/deleteTokesUser',formData)).toPromise();
+    return (await this.api._createData('admin/deleteTokenUser',formData)).toPromise();
   }
   
-  async editTokesUser(tokensUser:TokensUser){
+  async editTokenUser(tokensUser:TokensUser){
     const formData = new FormData();
     formData.append('id', tokensUser.id.toString());
     formData.append('num_tokens', tokensUser.num_tokens.toString());
     formData.append('phase_tokens', tokensUser.phase_tokens.toString());
     formData.append('payed_date', tokensUser.payed_date || '');
-    return (await this.api._createData('admin/editTokesUser',formData)).toPromise();
+    return (await this.api._createData('admin/editTokenUser',formData)).toPromise();
   }
 
 }
