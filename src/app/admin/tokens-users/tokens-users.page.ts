10053380fcdcc218c71ca3ee46013c9e615247d8
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-13 20:35:20 +0100

    Added Create Call

--- src/app/admin/tokens-users/tokens-users.page.ts
+++ src/app/admin/tokens-users/tokens-users.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { TokensUsersService } from 'src/app/admin/services/tokens-users.service';
+import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
-import { TokensUser } from '../models/tokens-user';
+import { TokenCRUD, TokensUser } from '../models/tokens-user';
 import { EditTokensComponent } from './edit-tokens/edit-tokens.component';
 
 @Component({
@@ -12,22 +13,41 @@ import { EditTokensComponent } from './edit-tokens/edit-tokens.component';
 })
 export class TokensUsersPage implements OnInit {
 
+  isLoading: boolean = false;
+
   constructor(
-    private tokensUsersSvc:TokensUsersService
+      private tokensUsersSvc:TokensUsersService
     , public modalCtrl:ModalController
     , public utils:UtilitiesService
+    , private translateSvc: TranslateConfigService
     ) { }
 
   tokensUsers:TokensUser[]
 
   async ngOnInit() {
-    this.tokensUsers = await this.tokensUsersSvc.getTokensUsers()
+    this.search();
   }
 
   filter:string;
-  async search(event: any) {
-    this.filter = event.target.value;
+  async search(event?: any) {
+    this.isLoading = true;
+    this.filter = event?.target?.value || '';
     this.tokensUsers = await this.tokensUsersSvc.getTokensUsers(this.filter);
+    this.isLoading = false;
+  }
+
+  async create() {
+    const suscribirseModal = await this.modalCtrl.create({
+      component: EditTokensComponent,
+      componentProps:{
+        tokenCRUD: TokenCRUD.Create
+      }
+    });
+    await suscribirseModal.present();
+
+    suscribirseModal.onDidDismiss().then(async (response) => {
+      if(response?.data?.updated) this.search();
+    })
   }
 
   async edit(tokensUser:TokensUser){
@@ -40,19 +60,26 @@ export class TokensUsersPage implements OnInit {
     await suscribirseModal.present();
 
     suscribirseModal.onDidDismiss().then(async (response) => {
-      if(response?.data?.updated) this.tokensUsers = await this.tokensUsersSvc.getTokensUsers(this.filter);
+      if(response?.data?.updated) this.search();
     })
   }
 
   async delete(tokensUser:TokensUser){
     try{
-      if(!await this.utils.confirm('admin.tokensUsers.deleteModal', {name: tokensUser.name})) return;
-      await this.tokensUsersSvc.deleteTokensUser(tokensUser.id);
-      this.utils.showToast('Tokens eliminados');
-      this.tokensUsers = await this.tokensUsersSvc.getTokensUsers(this.filter);
+      if(!await this.utils.confirm( 'admin.tokensUsers.modal', {
+        CRUD: this.translateSvc.instant( TokenCRUD.Delete ),
+        name: tokensUser?.name || tokensUser?.nick,
+        extra: this.translateSvc.instant( 'admin.tokensUsers.irreversible' )
+      })) return;
+      
+      this.isLoading = true; 
+      await this.tokensUsersSvc.deleteTokenUser(tokensUser.id);
+      this.utils.showToast( this.translateSvc.instant( 'admin.tokensUsers.delete.done' ));
+       this.search();
     }
     catch(e){
-      this.utils.showToast('No se pudo eliminar');
+      this.isLoading = false;
+      this.utils.showToast( this.translateSvc.instant( 'admin.tokensUsers.delete.error' ));
     }
   }
 
