3c43705db2b103d2242e97d2f5a95ebc4978294d
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-14 11:54:50 +0100

    Added Components Module

--- src/app/admin/tokens-users/tokens-users.module.ts
+++ src/app/admin/tokens-users/tokens-users.module.ts
@@ -9,6 +9,7 @@ import { TokensUsersPageRoutingModule } from './tokens-users-routing.module';
 import { TokensUsersPage } from './tokens-users.page';
 import { EditTokensComponent } from './edit-tokens/edit-tokens.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     FormsModule,
     IonicModule,
     TokensUsersPageRoutingModule,
-    SharedModule
+    SharedModule,
+    ComponentsModule
   ],
   declarations: [
     TokensUsersPage,
