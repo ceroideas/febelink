31eff6cbb7b16fd11107aeb32abc73147d00bc71
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-15 13:14:59 +0100

    Added Exchange and ReactiveForms

--- src/app/pages/wallet/wallet.module.ts
+++ src/app/pages/wallet/wallet.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { WalletPageRoutingModule } from './wallet-routing.module';
 
 import { WalletPage } from './wallet.page';
+import { ExchangeComponent } from './exchange/exchange.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, WalletPageRoutingModule],
-  declarations: [WalletPage],
+  imports: [
+      CommonModule
+    , FormsModule
+    , ReactiveFormsModule 
+    , IonicModule
+    , WalletPageRoutingModule
+  ],
+  declarations: [
+      WalletPage
+    , ExchangeComponent
+],
 })
 export class WalletPageModule {}
