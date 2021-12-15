e156b67d320f4a1e9f1953179350fe98e10c7a06
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-15 19:30:47 +0100

    Exchange Template

--- src/app/pages/wallet/exchange/exchange.component.ts
+++ src/app/pages/wallet/exchange/exchange.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import * as Currency from 'src/app/models/currency.model';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { CryptoCurrency } from 'src/app/models/currency.model';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { SelectAssetComponent } from '../select-asset/select-asset.component';
+import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
+import { UtilitiesService } from 'src/app/services/utilities.service';
 
 @Component({
   selector: 'app-exchange',
@@ -10,40 +13,93 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class ExchangeComponent implements OnInit {
 
-  currencyTypes = Currency.CryptoCurrencyTypes;
-  @Input() origin: Currency.CryptoCurrency;
-  @Input() destiny: Currency.CryptoCurrency;
+  @Input() origin: CryptoCurrency = {};
+  @Input() destiny: CryptoCurrency = {};
+  @Input() minnersFee: string = '0.000002 FLAU = $ 0.0447';
 
   public exchangeForm: FormGroup;
 
   constructor(
       private modalController: ModalController
     , private formBuilder: FormBuilder
-  ) { }
+    , private popoverController: PopoverController
+    , private translateSvc: TranslateConfigService
+    , private utilities: UtilitiesService
+  ) {}
 
   ngOnInit() {
-    this.builtForm();
+    this.buildForm();
   }
 
   ionViewDidLeave() {
     this.exchangeForm.reset();
   }
 
-  builtForm() {
+  buildForm() {
     this.exchangeForm = this.formBuilder.group({
-      num_origin: new FormControl( Validators.required ),
-      assetDestiny: new FormControl( Validators.required )
+      num_origin: new FormControl(( '' ), [ Validators.required ]),
+      num_destiny: new FormControl(( '' ), [ Validators.required ])
     });
   }
 
   onDismiss( ) {
     this.modalController.dismiss({ });
   }
+  
+  async selectAsset( isOrigin: boolean ) {
+    event.stopPropagation();
+    const popover = await this.popoverController.create({
+      component: SelectAssetComponent,
+      translucent: true,
+      mode: 'md',
+      componentProps: { except: !isOrigin ? this.origin.currency : this.destiny.currency }
+    });
 
-  originChange( event ){
-    this.origin.currency = event.detail.value;
+    await popover.present();
+
+    const { data } = await popover.onDidDismiss();
+
+    // Only do if asset selected
+    if( !data?.asset )
+      return;
+
+    // Assign asset to corresponding card
+    const asset = data.asset as CryptoCurrency;
+
+    if( isOrigin )
+      this.origin.currency = asset.currency;
+    else
+      this.destiny.currency = asset.currency;
   }
-  destinyChange( event ){
-    this.destiny.currency = event.detail.value;
+
+  exchange() {
+    if( !this.checkErrors() )
+      return;
+
+    this.modalController.dismiss({ origin: this.origin, destiny: this.destiny });
+  }
+
+  checkErrors(): boolean {
+    const { num_origin, num_destiny } = this.exchangeForm.value;
+
+    if( !this.destiny?.currency ) {
+      this.utilities.showToast( this.translateSvc.instant( 'pages.wallet.error.missing-destiny' ));
+      return false;
+    }
+
+    if( !num_origin || Number( num_origin ) <= 0 ) {
+      this.utilities.showToast( this.translateSvc.instant( 'pages.wallet.error.qOffer' ));
+      return false;
+    }
+    
+    if( !num_destiny || Number( num_destiny ) <= 0 ) {
+       this.utilities.showToast( this.translateSvc.instant( 'pages.wallet.error.qDemand' ));
+      return false;
+    }
+
+    this.origin.ammount = Number( num_origin );
+    this.destiny.ammount = Number( num_destiny );
+
+    return true;
   }
 }
