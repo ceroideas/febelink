2067d74923bf7d836eb522cffa7f1b2aa1681ef0
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-15 13:14:35 +0100

    enum const CurrencyTpes list

--- src/app/models/currency.model.ts
+++ src/app/models/currency.model.ts
@@ -10,3 +10,11 @@ export interface CryptoCurrency {
   currency: CryptoCurrencyType;
   ammount: number;
 }
+
+export const CryptoCurrencyTypes: CryptoCurrency[] = [
+  { currency: CryptoCurrencyType.aureo,  ammount: 0 },
+  { currency: CryptoCurrencyType.lumens,  ammount: 0 },
+  { currency: CryptoCurrencyType.euro,  ammount: 0 },
+  { currency: CryptoCurrencyType.uSDolar,  ammount: 0 },
+  { currency: CryptoCurrencyType.bitcoin,  ammount: 0 }
+];
