3ed1f79e128c7b6777936a881323016ef1048e2e
Abdias Natanael Vrech <abdias.dev@gmail.com>
2021-12-22 14:29:15 +0100

    Creation of Pipe to display dates correctly

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { FilterPipe } from '../pipes/filter.pipe';
 import { SafeHtmlPipe } from '../pipes/safehtml.pipe';
+import { DateFormatPipe } from './date-format';
 import { SplitNumberPipe } from './split-number.pipe';
 import { TypeofPipe } from './typeof.pipe';
 
 @NgModule({
   declarations: [
-    FilterPipe,
-    SafeHtmlPipe,
-    TypeofPipe,
-    SplitNumberPipe,
+      FilterPipe
+    , SafeHtmlPipe
+    , TypeofPipe
+    , SplitNumberPipe
+    , DateFormatPipe
   ],
   imports: [],
   exports: [
-    FilterPipe,
-    SafeHtmlPipe,
-    TypeofPipe,
-    SplitNumberPipe,
+      FilterPipe
+    , SafeHtmlPipe
+    , TypeofPipe
+    , SplitNumberPipe
+    , DateFormatPipe
   ],
   entryComponents: [],
 })
