3a2697b26eeed5700a7cd8ed06983be8edeb9c47
Abdias Natanael Vrech <abdias.dev@gmail.com>
2022-01-04 13:24:09 +0100

    Changed Text and btn to Register, removed Input

--- src/app/landing/shared/whitelist/whitelist.component.ts
+++ src/app/landing/shared/whitelist/whitelist.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { ILang, ILangDEFAULTS } from 'src/app/models/langs.model';
+import { IUser } from 'src/app/models/user.model';
 import { ApiService } from 'src/app/services/api.service';
 import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
@@ -12,15 +14,22 @@ import { UtilitiesService } from 'src/app/services/utilities.service';
   styleUrls: ['./whitelist.component.scss'],
 })
 export class WhitelistComponent implements OnInit {
+
+  user: IUser;
+
   constructor(
-    private api: ApiService,
-    private utils: UtilitiesService,
-    private translateService: TranslateConfigService
+      private api: ApiService
+    , private utils: UtilitiesService
+    , private translateService: TranslateConfigService
+    , private router: Router
   ) {}
   
   async ngOnInit() {
     this.lang = this.lang ? this.lang :
       (<ILang> await ILangDEFAULTS.getCurrentLang( this.translateService )).lang;
+
+    // ToDO: uncomment this line if should not show register button when logged
+    // this.user = await this.utils.getUserData();
   }
 
   @Input() lang: string;
@@ -50,5 +59,9 @@ export class WhitelistComponent implements OnInit {
       this.utils.dismissLoading();
     }
     console.log(this.response);
+  }
+
+  register() {
+    this.router.navigate(['registro']);
   }
 }
