de7faed274e3e34e3b25f91eae12d1346b4b37f0
abdias.dev <abdias.dev@gmail.com>
2022-01-29 08:22:18 +0100

    Assets Service

---
+++ src/app/services/wallet/asset.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { CryptoCurrency } from 'src/app/models/wallet/currency.model';
+import { SelectAssetComponent } from 'src/app/pages/wallet/select-asset/select-asset.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AssetService {
+
+    constructor(
+        private popCtrl: PopoverController
+    ) {}
+
+    getImg( asset: string ) {
+        switch( ( asset || '' ).toUpperCase() ) {
+            case 'FLAU':
+            case 'FBLINKCOINV3':
+                return 'flau';
+            default:
+                return 'xlm';
+        }
+    }
+
+    getColor( asset: string ) {
+        switch( ( asset || '' ).toUpperCase() ) {
+            case 'FLAU':
+            case 'FBLINKCOINV3':
+                return 'febelink';
+            default:
+                return 'black';
+        }
+    }
+
+    async select(
+        assetTpes: CryptoCurrency[],
+        exceptCrypto?: CryptoCurrency
+    ): Promise<CryptoCurrency>
+    {
+        const popover = await this.popCtrl.create({
+            component: SelectAssetComponent,
+            translucent: true,
+            mode: 'md',
+            componentProps: {
+                assetTypes: assetTpes,
+                except: exceptCrypto?.currency
+            }
+        });
+    
+        await popover.present();
+    
+        const { data } = await popover.onDidDismiss();
+
+        return new Promise( resolve => { resolve( data?.asset as CryptoCurrency )});
+    }
+}
