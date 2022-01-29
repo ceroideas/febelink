565aa0b39051568fa69eb5e53aad1c31027f6961
abdias.dev <abdias.dev@gmail.com>
2022-01-29 12:57:13 +0100

    New Fun Added

--- src/app/pages/wallet/offers-list/offers-list.component.ts
+++ src/app/pages/wallet/offers-list/offers-list.component.ts
@@ -1,13 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CryptoCurrency } from 'src/app/models/wallet/currency.model';
-import { Asset, Offer } from 'src/app/models/wallet/offers.models';
+import { CryptoCurrency, CryptoCurrencyType } from 'src/app/models/wallet/currency.model';
+import { OffersFilter, OffersList, OffersType } from 'src/app/models/wallet/offers.models';
 import { AssetService } from 'src/app/services/wallet/asset.service';
 import { OfferService } from 'src/app/services/wallet/offer.service';
-
-export enum OffersType {
-  MARKET = "market",
-  OWN = "own"
-}
 
 @Component({
   selector: 'app-offers-list',
@@ -15,60 +10,100 @@ export enum OffersType {
   styleUrls: ['./offers-list.component.scss'],
 })
 export class OffersListComponent implements OnInit {
-
-  @Input() offersMarket: Offer[];
-  @Input() offersOwn: Offer[];
-  @Input() type: OffersType = OffersType.MARKET;
-  offersType = OffersType;
   
   // Assets Available for the user to use
   @Input() assetTypes: CryptoCurrency[] = [];
 
+  // User Public Key
+  @Input() public: string;
 
-  // This array is to store the last id per page to go back on prev_page (pagination)
-  lastIdsPerPage: string[];
-
-  orderAsc: boolean = true;
-  assetSelling: CryptoCurrency;
-  assetBuying: CryptoCurrency;
-
-  // Segmentation | Tab | Page displaying
-  segment: string;
+  offersList: OffersList[] = [
+    { type: OffersType.MARKET, offers: [], lastIdsPerPage: [], isLoading: true },
+    { type: OffersType.OWN, offers: [], lastIdsPerPage: [], isLoading: true }
+  ]
   
+  // Segmentation | Tab | Page displaying
+  segment: OffersType = OffersType.MARKET;
+  offersType = OffersType;
+
+  // Sort Options
+  orderAsc: boolean = false;
+  assetSelling: CryptoCurrency = { currency: CryptoCurrencyType.aureo };
+  assetBuying: CryptoCurrency;
 
   constructor(
       private offerSvc: OfferService
     , private assetSvc: AssetService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit()
+  {
+    this.segmentChanged();
+  }
 
-  async selectAsset( isOwn: boolean ) {
+  async segmentChanged( event? )
+  {
+    const list = this.getList();
+
+    if( !list?.offers || list?.offers?.length == 0 )
+      this.refresh( list );
+  }
+
+  async selectAsset()
+  {
     const asset = await this.assetSvc.select( this.assetTypes );
+    if( !asset || asset == this.assetSelling )
+      return
 
+    this.assetSelling = asset;
+    this.refresh();
   }
 
-  async refresh( offers: Offer[] ) {
-    const list = await this.offerSvc.list({
-        buying: {
-          asset_code: this.assetBuying?.assetId
-        } as Asset,
+  order()
+  {
+    this.orderAsc = !this.orderAsc;
+    this.refresh();
+  }
 
-        selling: {
-          asset_code: this.assetSelling?.assetId
-        } as Asset,
-        paging_token: this.paginToken( offers )
-      } as Offer
+  async refresh( list?: OffersList )
+  {
+    if( !list )
+      list = this.getList();
+    
+      console.log( 'list: ', list );
+    list.isLoading = true;
+    list.offers = await this.offerSvc.list({
+        last_item: list?.lastIdsPerPage?.length > 0 ?
+          list.lastIdsPerPage[ list?.lastIdsPerPage?.length -1 ] : null,
+
+        selling: this.assetSelling?.assetId,
+
+        buying: this.assetBuying?.assetId,
+
+        account: list.type != OffersType.OWN ? null : this.public
+      } as OffersFilter,
     );
+    console.log( 'answer: ', list.offers );
+    list.isLoading = false;
   }
 
-  paginToken( list: Offer[] ) {
-    if( list.length > 0 ) {
-      const lastId = list[ list.length -1 ]?.paging_token;
-      this.lastIdsPerPage.push( lastId );
+  paginToken( list: OffersList )
+  {
+    if( list?.offers?.length > 0 ) {
+      const lastId = list[ list?.offers?.length -1 ]?.paging_token;
+      list.lastIdsPerPage.push( lastId );
       return lastId;
     }
 
+    return null;
+  }
+
+  getList()
+  {
+    for( let i = 0; i < this.offersList?.length; i++ )
+      if( this.offersList[ i ].type == this.segment )
+        return this.offersList[ i ];
+        
     return null;
   }
 }
