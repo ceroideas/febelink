11c7f492f491d19e979d10811bfc8ec3eb1fd8e1
abdias.dev <abdias.dev@gmail.com>
2022-02-01 16:03:13 +0100

    Add ExchangeSvc n Delete

--- src/app/pages/wallet/offers/offers.component.ts
+++ src/app/pages/wallet/offers/offers.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController, PopoverController } from '@ionic/angular';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Offer } from 'src/app/models/wallet/offers.models';
+import { WalletParams } from 'src/app/models/wallet/params.model';
+import { DateFormatType, MonthFormatType } from 'src/app/pipes/date-format.pipe';
 import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
 import { AssetService } from 'src/app/services/wallet/asset.service';
+import { ExchangeService, ExchangeType } from 'src/app/services/wallet/exchange.service';
 import { OfferService } from 'src/app/services/wallet/offer.service';
 
 @Component({
@@ -15,27 +17,70 @@ export class OffersComponent implements OnInit {
 
   @Input() offer: Offer;
   @Input() isSelling: boolean;
+  @Input() walletParams: WalletParams;
+  @Output() OnChange: EventEmitter<ExchangeType> = new EventEmitter()
 
+  dateFormatType = DateFormatType;
+  monthFormatType = MonthFormatType;
+  
   constructor(
-      private modalController: ModalController
-    , private popCtrl: PopoverController
+      private utils: UtilitiesService
     , private translateSvc: TranslateConfigService
-    , private utilities: UtilitiesService
     , private offerSvc: OfferService
     , public assetSvc: AssetService
+    , private exchangeSvc: ExchangeService
   ) {}
 
   ngOnInit() {}
   
-  buy() {
-    this.offerSvc.buy( this.offer );
+  async buy() {
+    const response = await this.exchangeSvc.show(
+      ExchangeType.BUY, this.walletParams, { offer: this.offer }
+    );
+    console.log( 'response: ', response );
+    this.OnChange.emit( ExchangeType.EDIT );
   }
 
-  edit() {
-
+  async edit() {
+    const response = await this.exchangeSvc.show(
+      ExchangeType.EDIT, this.walletParams, { offer: this.offer }
+    );
+    console.log( 'response: ', response );
+    this.OnChange.emit( ExchangeType.EDIT );
   }
 
-  delete() {
-
+  async delete() {
+    this.utils.showAlert(
+      this.translateSvc.instant( 'pages.wallet.offers.delete.title' ),
+      this.translateSvc.instant( 'pages.wallet.offers.delete.message' ),
+      '',
+      [
+          // Cancel
+        { text: this.translateSvc.instant( 'common.buttons.cancel' ), role: 'cancel' },
+        { // Continue
+          text: this.translateSvc.instant( 'common.buttons.continue' ),
+          handler: () => this.doDelete(),
+        },
+      ]
+    );
+  }
+  private async doDelete( ) {
+    await this.utils.showLoading();
+    this.offerSvc.delete( this.offer ).then(
+      (response) => {
+        console.log( 'response: ', response );
+        this.utils.showToast( this.translateSvc.instant( 'pages.wallet.offers.delete.success' ));
+        this.utils.dismissLoading();
+        this.OnChange.emit( ExchangeType.DELETE );
+      },
+      (err) => {
+        console.log( 'error: ', err );
+        this.utils.dismissLoading();
+        this.utils.showAlert(
+          this.translateSvc.instant( 'pages.wallet.offers.delete.title' ),
+          this.translateSvc.instant( 'pages.wallet.offers.delete.error' ) + err.message
+        );
+      }
+    );
   }
 }
