f05d7750f5e7ef83f9bedfc3bc0adf7f2c0019b6
abdias.dev <abdias.dev@gmail.com>
2022-02-04 16:00:39 +0100

    Fixes
    Replaced Utils with new Services
    Handle Error on Request

--- src/app/pages/wallet/offers/offers.component.ts
+++ src/app/pages/wallet/offers/offers.component.ts
@@ -3,10 +3,13 @@ import { Offer } from 'src/app/models/wallet/offers.models';
 import { WalletParams } from 'src/app/models/wallet/params.model';
 import { DateFormatType, MonthFormatType } from 'src/app/pipes/date-format.pipe';
 import { TranslateConfigService } from 'src/app/services/translate/translate-config.service';
-import { UtilitiesService } from 'src/app/services/utilities.service';
 import { AssetService } from 'src/app/services/wallet/asset.service';
-import { ExchangeService, ExchangeType } from 'src/app/services/wallet/exchange.service';
+import { ExchangeService } from 'src/app/services/wallet/exchange.service';
 import { OfferService } from 'src/app/services/wallet/offer.service';
+import { ExchangeType } from 'src/app/models/wallet/exchange.model';
+import { AlertSvc } from 'src/app/services/alert.service';
+import { ToastSvc } from 'src/app/services/toast.service';
+import { LoadingSvc } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-offers',
@@ -24,63 +27,66 @@ export class OffersComponent implements OnInit {
   monthFormatType = MonthFormatType;
   
   constructor(
-      private utils: UtilitiesService
-    , private translateSvc: TranslateConfigService
-    , private offerSvc: OfferService
+      private translateSvc: TranslateConfigService
+    , public offerSvc: OfferService
     , public assetSvc: AssetService
     , private exchangeSvc: ExchangeService
+    , private alertSvc: AlertSvc
+    , private toastSvc: ToastSvc
+    , private loadingSvc: LoadingSvc
   ) {}
 
   ngOnInit() {}
   
   async buy() {
-    const response = await this.exchangeSvc.show(
+    const { saved, response, error } = await this.exchangeSvc.show(
       ExchangeType.BUY, this.walletParams, { offer: this.offer }
     );
-    console.log( 'response: ', response );
-    this.OnChange.emit( ExchangeType.BUY );
+
+    console.log( 'response: ', response, ' || error: ', error );
+    if( saved && !error ) this.OnChange.emit( ExchangeType.BUY );
   }
 
   async edit() {
-    const response = await this.exchangeSvc.show(
+    const { saved, response, error } = await this.exchangeSvc.show(
       ExchangeType.EDIT, this.walletParams, { offer: this.offer }
     );
-    console.log( 'response: ', response );
-    this.OnChange.emit( ExchangeType.EDIT );
+    
+    console.log( 'response: ', response, ' || error: ', error );
+    if( saved && !error ) this.OnChange.emit( ExchangeType.EDIT );
   }
 
   async delete() {
-    this.utils.showAlert(
-      this.translateSvc.instant( 'pages.wallet.offers.delete.title' ),
-      this.translateSvc.instant( 'pages.wallet.offers.delete.message' ),
-      '',
-      [
+    this.alertSvc.show({
+      title: 'pages.wallet.offers.delete.title',
+      msg: 'pages.wallet.offers.delete.message',
+      btns: [
           // Cancel
-        { text: this.translateSvc.instant( 'common.buttons.cancel' ), role: 'cancel' },
+        { text: 'common.buttons.cancel', role: 'cancel' },
         { // Continue
-          text: this.translateSvc.instant( 'common.buttons.continue' ),
+          text: 'common.buttons.continue',
           handler: () => this.doDelete(),
         },
       ]
-    );
+    }, true );
   }
   private async doDelete( ) {
-    await this.utils.showLoading();
-    this.offerSvc.delete( this.offer ).then(
-      (response) => {
-        console.log( 'response: ', response );
-        this.utils.showToast( this.translateSvc.instant( 'pages.wallet.offers.delete.success' ));
-        this.utils.dismissLoading();
-        this.OnChange.emit( ExchangeType.DELETE );
-      },
-      (err) => {
-        console.log( 'error: ', err );
-        this.utils.dismissLoading();
-        this.utils.showAlert(
-          this.translateSvc.instant( 'pages.wallet.offers.delete.title' ),
-          this.translateSvc.instant( 'pages.wallet.offers.delete.error' ) + err.message
-        );
-      }
-    );
+    await this.loadingSvc.show();
+    const { response, error } = await this.offerSvc.delete( this.offer );
+
+    if( error ) {
+      console.log( 'error: ', error );
+      this.loadingSvc.dismiss();
+      this.alertSvc.show({
+        title: 'pages.wallet.offers.delete.title',
+        message: this.translateSvc.instant( 'pages.wallet.offers.delete.error' ) + error.message
+      }, true );
+      return
+    }
+    
+    console.log( 'response: ', response );
+    this.toastSvc.show( 'pages.wallet.offers.delete.success', true );
+    this.loadingSvc.dismiss();
+    this.OnChange.emit( ExchangeType.DELETE );
   }
 }
