78ecfe66623c00600dc1fa3a4dfd34525434d0f7
abdias.dev <abdias.dev@gmail.com>
2022-02-09 18:47:27 +0100

    Changes to Exchange
    Adapted Horizontally for desktop
    Added Conversion
    Added Market Price
    Added InputChange Listener
    Made both -> for popup and for template
    Renamed origin n destiny
    Added listener on Done
    Added Available qant
    Reset form on done
    Added default assets selected
    Added Delete when editing

--- src/app/pages/wallet/exchange/exchange.component.ts
+++ src/app/pages/wallet/exchange/exchange.component.ts
@@ -1,49 +1,72 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { CryptoCurrency, CryptoCurrencyType } from 'src/app/models/wallet/currency.model';
+import { AlertSvc } from './../../../services/alert.service';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { CryptoCurrency } from 'src/app/models/wallet/currency.model';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { TokensUser } from 'src/app/admin/models/tokens-user';
 import { AssetService } from 'src/app/services/wallet/asset.service';
 import { TwoFAService } from 'src/app/services/two_fa.service';
-import { Offer } from 'src/app/models/wallet/offers.models';
-import { ExchangeType } from 'src/app/models/wallet/exchange.model';
+import { MarketPrice, Offer } from 'src/app/models/wallet/offers.models';
+import { ExchangeInput, ExchangeType } from 'src/app/models/wallet/exchange.model';
 import { OfferService } from 'src/app/services/wallet/offer.service';
 import { ToastSvc } from 'src/app/services/toast.service';
 import { KycPopSvc } from 'src/app/services/kyc/kyc.pop.service';
+import { DateFormatType } from 'src/app/pipes/date-format.pipe';
+import { ExchangeInputSvc } from 'src/app/services/wallet/exchange.input.service';
+import { CloneAssetSvc } from '../../../services/wallet/clone.assets.service';
+import { ExchangeService } from 'src/app/services/wallet/exchange.service';
+import { WalletParams } from 'src/app/models/wallet/params.model';
+import { TkLimitSvc } from 'src/app/services/wallet/tk-limit.service';
 
 @Component({
   selector: 'app-exchange',
   templateUrl: './exchange.component.html',
   styleUrls: ['./exchange.component.scss'],
 })
-export class ExchangeComponent implements OnInit {
+export class ExchangeComponent implements OnInit, OnChanges {
+
 
   @Input() offer: Offer;
-  @Input() origin: CryptoCurrency = {};
-  @Input() destiny: CryptoCurrency = {};
-  @Input() minnersFee: string;
+  @Input() sell: CryptoCurrency = {};
+  @Input() buy: CryptoCurrency = {};
   @Input() kycVerified: boolean = true;
   @Input() exchangeType: ExchangeType = ExchangeType.CREATE;
   
-  @Input() userWallets: CryptoCurrency[] = [];
-  @Input() retainedTks: TokensUser[];
-  @Input() assetsMaxDecimals: number;
+  @Input() walletParams: WalletParams = {}
+  
+  @Input() useMarketPrice: boolean = false;
+  mktPrice: MarketPrice = { isLoading: true };
+  conversionDisabled: boolean;
+
+  @Output() OnDismiss: any
+  @Output() OnDone: EventEmitter<Object> = new EventEmitter()
   
   public form: FormGroup;
-  exchangesType = ExchangeType;
+  exchangeTypes = ExchangeType;
+  exchangesInput = ExchangeInput;
+  dateFormatType = DateFormatType;
+
+  limitSell: number
+  limitBuy: number
 
   constructor(
-      private modalController: ModalController
-    , private formBuilder: FormBuilder
+      private formBuilder: FormBuilder
     , private toastSvc: ToastSvc
     , public assetSvc: AssetService
     , private twoFASvc: TwoFAService
     , private offerSvc: OfferService
+    , private exchangeInputSvc: ExchangeInputSvc
     , private kycPopSvc: KycPopSvc
+    , private cloneAssetSvc: CloneAssetSvc
+    , private alertSvc: AlertSvc
+    , private exchangeSvc: ExchangeService
+    , private tkLimitSvc: TkLimitSvc
   ) {}
 
   ngOnInit() {
-    this.buildForm();
+    this.setDefaults();
+  }
+  
+  ngOnChanges( changes: SimpleChanges ) {
+    this.setDefaults();
   }
 
   ionViewDidLeave() {
@@ -51,31 +74,78 @@ export class ExchangeComponent implements OnInit {
   }
 
   buildForm() {
+    this.conversionDisabled = this.exchangeType == ExchangeType.BUY || this.useMarketPrice;
+    const isBuy = this.exchangeType == ExchangeType.BUY;
+
     this.form = this.formBuilder.group({
-      num_sell: new FormControl(( this.origin?.amount > 0 ? this.origin?.amount : '' ), [ Validators.required ]),
-      num_buy: new FormControl(( this.destiny?.amount > 0 ? this.destiny?.amount : '' ), [ Validators.required ])
+        num_sell_qant: new FormControl(
+          ( this.sell?.amount == 0 ? '' : this.sell?.amount )
+          , [ Validators.required ]
+        )
+        , num_sell_conv: new FormControl({
+          value: this.sell?.amount == 0 ? '' : ( !isBuy ? 1 : this.offer?.price )
+          , disabled: this.conversionDisabled
+        })
+        , num_buy_qant: new FormControl(
+          ( this.buy?.amount == 0 ? '' : this.buy?.amount )
+          , [ Validators.required ]
+        )
+        , num_buy_conv: new FormControl({
+          value: this.buy?.amount == 0 ? '' : ( isBuy ? 1 : this.offer?.price )
+          , disabled: this.conversionDisabled
+        })
     });
   }
 
-  onDismiss( ) {
-    this.modalController.dismiss({ });
+  // If Creating Exchange, no default asset selected, so pick one by default
+  setDefaults() {
+    if( !this.walletParams ) return;
+    
+    this.exchangeSvc.setType( this.exchangeType )
+    this.exchangeSvc.setParams( this.walletParams )
+
+    this.cloneAssetSvc.set( this.exchangeType , this.offer, this.walletParams )
+    
+    this.sell = this.cloneAssetSvc.get( this.sell, this.buy, true )
+    this.buy = this.cloneAssetSvc.get( this.buy, this.sell, false )
+    
+    this.setLimits();
+    this.buildForm();
+  }
+
+  setLimits() {
+    this.limitSell = this.tkLimitSvc.qantAvailable( this.sell, this.walletParams )
+    this.limitBuy = this.tkLimitSvc.qantAvailable( this.buy, this.walletParams )
+    this.marketPrice()
+  }
+
+  dismiss( object? ) {
+    if( object?.saved ) {
+      this.OnDone.emit( object )
+      this.form?.reset()
+    }
+
+    if( this.OnDismiss )
+      this.OnDismiss( object );
   }
   
-  async selectAsset( event, isOrigin: boolean ) {
+  async selectAsset( event, isSelling: boolean ) {
     const asset = await this.assetSvc.select(
           event
-        , this.userWallets
-        , !isOrigin ? this.origin : this.destiny
+        , this.walletParams.userWallets
+        , !isSelling ? this.sell : this.buy
     );
 
     // Only do if asset selected
     if( !asset )
       return;
 
-    if( isOrigin )
-      this.origin.currency = asset.currency;
+    if( isSelling )
+      this.sell.currency = asset.currency;
     else
-      this.destiny.currency = asset.currency;
+      this.buy.currency = asset.currency;
+
+    this.setLimits();
   }
 
   switchAssets() {
@@ -85,12 +155,13 @@ export class ExchangeComponent implements OnInit {
     }
 
     const selling: CryptoCurrency =
-      { currency: this.destiny?.currency, assetId: this.destiny?.assetId, issuerId: this.destiny?.issuerId };
+      { currency: this.buy?.currency, assetId: this.buy?.assetId, issuerId: this.buy?.issuerId };
     const buying: CryptoCurrency =
-      { currency: this.origin?.currency, assetId: this.origin?.assetId, issuerId: this.origin?.issuerId };
+      { currency: this.sell?.currency, assetId: this.sell?.assetId, issuerId: this.sell?.issuerId };
     
-    this.origin = selling;
-    this.destiny = buying;
+    this.sell = selling;
+    this.buy = buying;
+    this.setLimits()
   }
 
   async exchange() {
@@ -105,83 +176,135 @@ export class ExchangeComponent implements OnInit {
     }
     
     if( await this.twoFASvc.verify() )
-      this.modalController.dismiss({ origin: this.origin, destiny: this.destiny });
+      this.OnDo({ sell: this.sell, buy: this.buy });
   }
 
   checkErrors(): boolean {
-    const { num_sell, num_buy } = this.form.value;
+    const { num_sell_qant, num_buy_qant } = this.form.value;
 
-    if( !this.origin?.currency ) {
-      this.toastSvc.show( 'pages.wallet.error.missing-origin', true );
+    if( !this.sell?.currency ) {
+      this.toastSvc.show( 'pages.wallet.error.missing-sell', true );
       return false;
     }
 
-    if( !this.destiny?.currency ) {
-      this.toastSvc.show( 'pages.wallet.error.missing-destiny', true );
+    if( !this.buy?.currency ) {
+      this.toastSvc.show( 'pages.wallet.error.missing-buy', true );
       return false;
     }
 
-    if( !num_sell || Number( num_sell ) <= 0 ) {
-      this.toastSvc.show( 'pages.wallet.error.qOffer', true );
+    if( !num_sell_qant || Number( num_sell_qant ) <= 0 ) {
+      this.toastSvc.show( 'pages.wallet.error.sell_qant', true );
       return false;
     }
     
-    if( !num_buy || Number( num_buy ) <= 0 ) {
-       this.toastSvc.show( 'pages.wallet.error.qDemand', true );
+    if( !num_buy_qant || Number( num_buy_qant ) <= 0 ) {
+       this.toastSvc.show( 'pages.wallet.error.buy_qant', true );
       return false;
     }
 
-    if( !this.hasThatAmount( num_sell ))
+    if( !this.hasThatAmount( num_sell_qant ))
       return false;
 
-    this.origin.amount = Number( num_sell );
-    this.destiny.amount = Number( num_buy );
+    this.sell.amount = Number( num_sell_qant );
+    this.buy.amount = Number( num_buy_qant );
 
     return true;
   }
 
   hasThatAmount( amount: number ): boolean {
-    for( let i = 0; i < this.userWallets?.length; i++ ) {
-      const asset = this.userWallets[ i ];
-      if( asset?.currency === this.origin?.currency ) {
-        let retained: number = 0;
-        
-        // Substract the retained assets amount
-        switch( asset?.currency ) {
-          case CryptoCurrencyType.aureo:
-            this.retainedTks?.forEach( tk => retained += Number( tk?.num_tokens || '0' ));
-            break;
-        }
-
-        if( asset.amount - retained >= amount )
-          return true;
-      }
-    }
+    if( !this.tkLimitSvc.exceeds( amount, this.sell, this.walletParams ))
+      return true;
 
     this.toastSvc.show( 'pages.wallet.error.exceeded', true );
     return false;
   }
 
-  qantChange( input, isSell: boolean ) {
-    const value = Number.parseFloat( input.value || '1' );
-    if( this.exchangeType != ExchangeType.BUY )
-      return;
+  async marketPrice() {
+    if( this.exchangeType == ExchangeType.BUY || !this.sell?.currency || !this.buy?.currency ) return
 
-    const priceSell = Number.parseFloat( this.offer?.amount );
-    const priceBuy = this.offerSvc.calcBuy( this.offer?.amount, this.offer?.price, this.assetsMaxDecimals );
+    this.mktPrice.isLoading = true;
+    const { response, error } = await this.offerSvc.marketPrice( this.sell, this.buy )
+    
+    this.mktPrice = {
+      isLoading: false
 
-    this.checkQantExceeded( input, isSell ? priceBuy : priceSell, isSell )
+      , selling: response?.selling
+      , sellingIssuerId: response?.sellingIssuerId
+      , price_selling: response?.price_selling
+      
+      , buying: response?.buying
+      , buyingIssuerId: response?.buyingIssuerId
+      , price_buying: response?.price_buying
+    }
 
-    // Update opposite input
-    const key = isSell ? 'num_buy' : 'num_sell';
-    const opposite = ( isSell ? priceSell / priceBuy : priceBuy / priceSell ) * ( input?.value || 1 );
-    this.form.patchValue({ [key] : opposite?.toFixed( this.assetsMaxDecimals ) });
+    /* if( !error )
+      this.OnMarketPriceChecked( this.useMarketPrice ) */
   }
 
-  checkQantExceeded( input, qant: number, isSell: boolean ) {
-    if( Number.parseFloat( input.value || '0' ) > qant ) {
-      input.value = qant;
-      this.toastSvc.show( 'pages.wallet.exchange.exceeds-' + ( isSell ? 'sell' : 'buy' ), true );
+  OnMarketPriceChecked()
+  {
+    this.conversionDisabled = this.useMarketPrice;
+
+    if( this.useMarketPrice ) {
+      this.form.patchValue({ num_sell_conv: this.mktPrice?.price_selling })
+      this.form.patchValue({ num_buy_conv: this.mktPrice?.price_buying })
+      
+      this.qantChange()
+
+      this.form.controls['num_sell_conv'].disable()
+      this.form.controls['num_buy_conv'].disable()
+    } else {
+      this.form.controls['num_sell_conv'].enable()
+      this.form.controls['num_buy_conv'].enable()
     }
+  }
+  calcBuy(): number {
+    return this.offerSvc.calcBuy( this.offer?.amount, this.offer?.price, this.walletParams.assetsMaxDecimals )
+  }
+  qantChange( exchangeInput: ExchangeInput = ExchangeInput.SELL_CONV ) {
+    const { num_sell_qant, num_sell_conv, num_buy_qant, num_buy_conv } = this.form.getRawValue();
+    
+    const maxSell = this.exchangeType != ExchangeType.BUY
+      ? undefined
+      : Number.parseFloat( this.offer?.amount )
+    
+    const maxBuy = this.exchangeType != ExchangeType.BUY
+      ? undefined
+      : this.offerSvc.calcBuy( this.offer?.amount, this.offer?.price, this.walletParams.assetsMaxDecimals )
+
+    this.exchangeInputSvc.calc({
+        sell_qant: num_sell_qant
+      , sell_conv: num_sell_conv
+      , buy_qant: num_buy_qant
+      , buy_conv: num_buy_conv
+
+      , is: exchangeInput
+      , maxDecimals: this.walletParams.assetsMaxDecimals
+
+      , maxSell: maxSell
+      , maxBuy: maxBuy
+    }, this.form );
+  }
+
+  async delete() {
+    this.alertSvc.show({
+      title: 'pages.wallet.offers.delete.title',
+      msg: 'pages.wallet.offers.delete.message',
+      btns: [
+          // Cancel
+        { text: 'common.buttons.cancel', role: 'cancel' },
+        { // Continue
+          text: 'common.buttons.continue',
+          handler: () => this.OnDo({ delete: true }),
+        },
+      ]
+    }, true );
+  }
+
+  private async OnDo( data ) {
+    const { saved, response, error } = await this.exchangeSvc.OnDone( data, this.offer );
+
+    if( !error )
+      this.dismiss({ saved, response, error });
   }
 }
