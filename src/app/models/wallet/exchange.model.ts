2534a016e0317733c59fffe29481838ac4ce7ac4
abdias.dev <abdias.dev@gmail.com>
2022-02-09 18:41:39 +0100

    added input enum n interface

--- src/app/models/wallet/exchange.model.ts
+++ src/app/models/wallet/exchange.model.ts
@@ -1,6 +1,31 @@
 export enum ExchangeType {
-    CREATE = 'create',
-    EDIT = 'edit',
-    BUY = 'purchase',
-    DELETE = 'delete'
+    CREATE = 'create'
+  , MARKET = 'market' // Use Market Price on Create
+  , EDIT = 'edit'
+  , BUY = 'purchase'
+  , DELETE = 'delete'
+}
+
+export enum ExchangeInput {
+    SELL_QANT = 'sell_qant'
+  , SELL_CONV = 'sell_conversion'
+  , BUY_QANT  = 'buy_qant'
+  , BUY_CONV  = 'buy_conversion'
+}
+
+export interface iExchangeInput {
+    sell_qant: string
+  , sell_conv: string
+  , buy_qant: string
+  , buy_conv: string
+  
+  , is: ExchangeInput
+  , maxDecimals: number
+
+  , limitSell?: number
+  , limitBuy?: number
+
+  // If Buying, there is a limit
+  , maxSell?: number
+  , maxBuy?: number
 }
