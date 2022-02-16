28fe326c2d96c2269684568c40c541227cb9bb52
abdias.dev <abdias.dev@gmail.com>
2022-02-16 15:01:12 +0100

    Added Posts Route to app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -204,6 +204,11 @@ const routes: Routes = [
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
   },
+  {
+    path: 'posts',
+    loadChildren: () => import('./pages/posts/post.module').then( m => m.PostPageModule)
+  },
+
 
 
 ];
