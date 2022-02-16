b245669ba979c8030ebe65a36b44608ecea388f9
abdias.dev <abdias.dev@gmail.com>
2022-02-16 15:11:17 +0100

    Creation of Global Posts module

---
+++ src/app/pages/posts/post.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PostPageRoutingModule } from './post-routing.module';
+
+import { PostPage } from './post.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  imports: [
+      CommonModule
+    , FormsModule
+    , ReactiveFormsModule 
+    , IonicModule
+    , PostPageRoutingModule
+    , SharedModule
+  ],
+  declarations: [
+      PostPage
+  ]
+})
+export class PostPageModule {}
