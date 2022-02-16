87718cf83b8e93166bd426243c9b202b23d5b3ae
abdias.dev <abdias.dev@gmail.com>
2022-02-16 15:09:32 +0100

    Creation of Service to Show User Filter pop

---
+++ src/app/components/user/services/user-filter.pop.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { IAdviseFilter } from 'src/app/pages/posts/advises/models/advises.model';
+import { UserFilterComponent } from '../filter/filter.component';
+import { IUserItem } from '../models/user-item.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserFilterPopSvc {
+
+    constructor(
+        private modalCtrl: ModalController
+    ) {}
+
+    async show( urlPath?: string, params?: IAdviseFilter ): Promise<IUserItem>
+    {
+        const popover = await this.modalCtrl.create({
+          component: UserFilterComponent,
+          componentProps:{
+            urlPath: urlPath
+            , params: params
+          },
+          mode: 'md',
+          cssClass: 'pop-w-300 pop-h-400 pop-opacity pop-br-10',
+          backdropDismiss: false // To prevent user cancel on touch outside by error
+        });
+    
+        await popover.present();
+    
+        const { data } = await popover.onDidDismiss();
+        console.log({ data })
+        return new Promise( resolve => { resolve( data?.user ) });
+    }
+}
