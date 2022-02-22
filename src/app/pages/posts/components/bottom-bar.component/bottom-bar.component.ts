6cd50e250213774384a4e104218c1cb72ebc6696
abdias.dev <abdias.dev@gmail.com>
2022-02-22 19:56:44 +0100

    Creation of Bottom Bar for Reacts

---
+++ src/app/pages/posts/components/bottom-bar.component/bottom-bar.component.ts
@@ -0,0 +1,70 @@
+import { AdviseService } from './../../advises/services/advises.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DateFormatType } from 'src/app/pipes/date-format';
+import { ShareService } from 'src/app/services/share.service';
+import { IAdviseFull } from '../../advises/models/advises.model';
+import { IUser } from 'src/app/models/user.model';
+import { UserSessionSvc } from 'src/app/services/user-session.service';
+
+@Component({
+  selector: 'app-post-bottom-bar-component',
+  templateUrl: './bottom-bar.component.html',
+  styleUrls: ['./bottom-bar.component.scss'],
+})
+export class PostBottomBarComponent implements OnInit
+{
+  @Input() id: number
+  @Input() post: IAdviseFull
+  @Input() showChat : boolean = false
+  @Input() showSeePost : boolean = false
+
+  user: IUser
+  dateFormatType = DateFormatType
+
+  constructor(
+      private router: Router
+    , private adviseSvc: AdviseService
+    , private shareSvc: ShareService
+    , private sessionSvc: UserSessionSvc
+  ) {}
+
+  ngOnInit() {}
+
+  async react()
+  {
+    if( !( await this.sessionSvc.checkLogged() )) return
+
+    this.post.reacted = !this.post.reacted
+
+    // ToDo: select reaction from popover
+    const reactType = 1;
+    this.adviseSvc.react( this.id, reactType, this.post.reacted ? 1 : 0 )
+
+    this.post.react_qant = ( this.post.react_qant || 0 ) + ( this.post.reacted ? 1 : -1 )
+  }
+
+  async share( ev: any )
+  {
+    if( await this.shareSvc.exec(
+        ev
+      , `posts/advise/${this.id}`
+      , this.post?.title
+      , this.post?.summary
+      , this.post.photo
+    )) {
+      this.post.shared = ( this.post?.shared || 0 ) + 1
+      this.adviseSvc.shared( this.id )
+    }
+  }
+
+  async chat()
+  {
+    if( !( await this.sessionSvc.checkLogged() )) return
+  }
+
+  watch()
+  {
+    this.router.navigate([ `posts/advise/${this.id}` ]);
+  }
+}
