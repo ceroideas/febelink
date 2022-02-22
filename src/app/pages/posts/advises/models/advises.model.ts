782238963d9762226ed4176b1ea0563b181aad87
abdias.dev <abdias.dev@gmail.com>
2022-02-22 19:55:55 +0100

    Issolated Interfaces

--- src/app/pages/posts/advises/models/advises.model.ts
+++ src/app/pages/posts/advises/models/advises.model.ts
@@ -1,36 +1,46 @@
 import { IPaginationFilter } from "src/app/models/pagination.model";
 
 export interface IAdvise {
-    id: number
-    uid: number
-    
+    id?: number
+    uid?: number
     lang: number
 
-    id_sector: number
-    sector: string
-
-    id_subsector: number
-    subsector: string
+    id_sector?: number
+    id_subsector?: number
 
     title: string
-    subtitle: string
+    subtitle?: string
     summary: string
-    content: string
+    content?: string
     
-    photo: string
-    video: string
+    photo?: string | ArrayBuffer
+    video?: string
     
-    shared: number
-    reacts: number
+    shared?: number
+    reacts?: number
 
-    created_at: string
-    updated_at: string
-    state: number
+    created_at?: string
+    updated_at?: string
+    id_state?: number
+    state?: string
+}
+
+export interface IAdviseFull extends IAdvise {
+    nick?: string
+    name?: string
+    lastName?: string
+    avatar?: string
+
+    sector: string | number
+    subsector?: string | number
+
+    react_qant?: number
+    reacted?: boolean
 }
 
 export interface IAdviseFilter extends IPaginationFilter {
-    sector?: number
-    subsector?: number
+    sector?: string | number
+    subsector?: string | number
     lang?: number
     user?: number
 }
