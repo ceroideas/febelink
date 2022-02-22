0baa7a58fb949b9d06dd1851ecfab08da6c34981
abdias.dev <abdias.dev@gmail.com>
2022-02-22 19:51:12 +0100

    Moved Imports to PostComponentsModule

--- src/app/pages/posts/advises/advise.module.ts
+++ src/app/pages/posts/advises/advise.module.ts
@@ -1,31 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { AdvisePageRoutingModule } from './advise-routing.module';
 
 import { AdviseCRUDPage } from './advise-crud/advise-crud.page';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { AdvisePage } from './advise/advise.page';
 import { AdvisesPage } from './advises/advises.page';
-import { AdviseComponent } from './components/advise/advise.component';
+import { PostComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
-      CommonModule
-    , FormsModule
-    , ReactiveFormsModule 
-    , IonicModule
-    , AdvisePageRoutingModule
-    , SharedModule
+    PostComponentsModule
   ],
   declarations: [
       AdvisesPage
     , AdvisePage
     , AdviseCRUDPage
-    , AdviseComponent
   ]
 })
 export class AdvisePageModule {}
