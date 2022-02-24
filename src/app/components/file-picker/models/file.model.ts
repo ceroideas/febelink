acca21fbb792a710e548fc6ee67fe150019d1caa
abdias.dev <abdias.dev@gmail.com>
2022-02-24 13:58:15 +0100

    Creation of iFile and FileMaxSize enum

--- src/app/components/file-picker/models/file.model.ts
+++ src/app/components/file-picker/models/file.model.ts
@@ -9,3 +9,16 @@ export enum FileElementType {
     , ION_IMG
     , VIDEO
 }
+export enum FileMaxSize { // In Bytes
+      BLOB_TINY = 255               // Up to 0.255 bytes ( 0.000255 MB )
+    , BLOB = 65535                  // Up to 64 Kb ( 0.064 MB )
+    , BLOB_MEDIUM = 16777215        // Up to 16 Mb
+    , BLOBL_LONG = 4294967295       // Up to 4 Gb
+    , MAX_ALLOWED_PACKET = 4194304  // Up to 4 Mb
+}
+
+export interface IFile {
+    type?: 'IFile'
+    src: string | ArrayBuffer
+    file: any
+}
