3f318cc092b4ffe732016e9a7747ee9269a0575f
abdias.dev <abdias.dev@gmail.com>
2022-03-02 10:37:28 +0100

    Oracles inside TabPage

--- src/app/pages/posts/post-routing.module.ts
+++ src/app/pages/posts/post-routing.module.ts
@@ -1,24 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TabsPage } from 'src/app/tabs/tabs.page';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oracles', pathMatch: 'full' },
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      { path: '', redirectTo: 'oracles', pathMatch: 'full' },
+    
+      /* Oracles List */
+      {
+        path: 'oraculos',
+        loadChildren: () => import('./advises/advise.module').then( m => m.AdvisePageModule )
+      },
+      {
+        path: 'oracles',
+        loadChildren: () => import('./advises/advise.module').then( m => m.AdvisePageModule )
+      },
+      
+      // Redirect Tabs when clicked
+      { path: 'todas', redirectTo: '/menu/todas', pathMatch: 'prefix' },
+      { path: 'busquedas', redirectTo: '/menu/busquedas', pathMatch: 'prefix' },
+      { path: 'ofertas', redirectTo: '/menu/ofertas', pathMatch: 'prefix' },
+      { path: 'perfil', redirectTo: '/menu/perfil', pathMatch: 'prefix' },
+      { path: 'welcome', redirectTo: '/menu/welcome', pathMatch: 'prefix' },
+      { path: 'notificaciones', redirectTo: '/menu/notificaciones', pathMatch: 'prefix' },
+    ]
+  },
 
-  /* Advises List */
+  // Individual Oracle
   {
     path: 'oraculo',
     loadChildren: () => import('./advises/advise.module').then( m => m.AdvisePageModule )
   },
   {
-    path: 'oraculos',
-    loadChildren: () => import('./advises/advise.module').then( m => m.AdvisePageModule )
-  },
-  {
     path: 'oracle',
-    loadChildren: () => import('./advises/advise.module').then( m => m.AdvisePageModule )
-  },
-  {
-    path: 'oracles',
     loadChildren: () => import('./advises/advise.module').then( m => m.AdvisePageModule )
   },
 ];
