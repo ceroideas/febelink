48f16f9a8662ea6d1a9f09bee8b01ecbb0632c00
abdias.dev <abdias.dev@gmail.com>
2022-03-02 10:45:04 +0100

    Added TabsSharedModule

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,13 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
-
 import { TabsPageRoutingModule } from './tabs-routing.module';
-
-import { TabsPage } from './tabs.page';
+import { TabsSharedModule } from './tabs.shared.module';
 
 @NgModule({
-  imports: [SharedModule, TabsPageRoutingModule],
-  declarations: [TabsPage],
+  imports: [TabsPageRoutingModule, TabsSharedModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TabsPageModule {}
