98044183ba181ece563fcc7fc513e377e8aaf7f2
abdias.dev <abdias.dev@gmail.com>
2022-03-07 16:14:52 +0100

    Included Sectors and Loading Module

--- src/app/pages/posts/advises/advise.module.ts
+++ src/app/pages/posts/advises/advise.module.ts
@@ -4,10 +4,14 @@ import { AdviseCRUDPage } from './advise-crud/advise-crud.page';
 import { AdvisePage } from './advise/advise.page';
 import { AdvisesPage } from './advises/advises.page';
 import { PostComponentsModule } from '../components/components.module';
+import { LoadingModule } from 'src/app/components/loading/loading.module';
+import { SectorsModule } from 'src/app/components/sectors/sectors.module';
 
 @NgModule({
   imports: [
-    PostComponentsModule
+      PostComponentsModule
+    , SectorsModule
+    , LoadingModule
   ],
   declarations: [
       AdvisesPage
