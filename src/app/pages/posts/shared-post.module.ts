5647577c5556eb4e72f3ca29bf6438448d696078
abdias.dev <abdias.dev@gmail.com>
2022-03-08 14:18:22 +0100

    Added PostSummary and ReactTypes component

--- src/app/pages/posts/shared-post.module.ts
+++ src/app/pages/posts/shared-post.module.ts
@@ -4,6 +4,8 @@ import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PostStatsComponent } from './advises/stats/stats.component';
+import { ReactTypesComponent } from './components/react-types/react-types.component';
+import { PostSummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   imports: [
@@ -14,9 +16,13 @@ import { PostStatsComponent } from './advises/stats/stats.component';
 ],
   exports: [
       PostStatsComponent
+    , PostSummaryComponent
+    , ReactTypesComponent
   ],
   declarations: [
       PostStatsComponent
+    , PostSummaryComponent
+    , ReactTypesComponent
   ],
 })
 export class SharedPostModule {}
