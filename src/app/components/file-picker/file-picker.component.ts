b8b6f20a2ff67497ff245387f5bb7f92e835b188
abdias.dev <abdias.dev@gmail.com>
2022-03-08 18:35:59 +0100

    Fix styles, Conditions n video on FilePicker

--- src/app/components/file-picker/file-picker.component.ts
+++ src/app/components/file-picker/file-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { FilePickType, FileElementType, IFile, FileMaxSize } from './models/file.model';
 import { FileService } from './services/file.service';
@@ -11,13 +11,13 @@ import { FileService } from './services/file.service';
 export class FilePickerComponent implements OnInit {
 
   @Input() src: string | ArrayBuffer
-  file: any
   @Input() fallback: string = 'assets/icon/svg/nopic.svg'
   @Input() height: string = '100%'
   @Input() width: string = '100%'
   @Input() maxSize: FileMaxSize = FileMaxSize.MAX_ALLOWED_PACKET
   @Input() styles: string = ''
   @Input() classes: string = ''
+  classWH: string = 'w-social-media h-social-media pointer '
 
   @Input() pickType: FilePickType = FilePickType.BOTH
     pickTypes = FilePickType
@@ -30,6 +30,9 @@ export class FilePickerComponent implements OnInit {
   isNative: boolean = false
 
   @ViewChild( "filePicker" ) filePicker: ElementRef
+  @ViewChild('videoPlayer') videoPlayer: ElementRef
+
+  iFile: IFile = {}
 
   constructor(
       public mediaSvc: FileService
@@ -40,21 +43,26 @@ export class FilePickerComponent implements OnInit {
     this.platform.ready().then(() => this.isNative = this.platform.is( 'cordova' ))
   }
 
+  ngOnChanges( changes: SimpleChanges ): void
+  {
+    if ( 'src' in changes) this.iFile.src = changes.src.currentValue
+  }
+
   clicks()
   {
     this.filePicker.nativeElement.click()
   }
 
-  async pickMedia( filePicker ) {
+  async pickMedia( filePicker )
+  {
     if( this.OnClick ) this.OnClick.emit( filePicker )
-    const { src, file } = await this.mediaSvc.pickImg( filePicker, this.maxSize )
-    this.src = src
-    this.file = file
-    if( this.src && this.OnFile ) this.OnFile.emit({ src, file })
+    this.iFile = await this.mediaSvc.pickImg( filePicker, this.maxSize )
+    if( this.iFile && this.OnFile ) this.OnFile.emit( this.iFile )
   }
 
   toggleVideo( event )
   {
-    event.toElement.play()
+    // event.nativeElement.play()
+    this.videoPlayer.nativeElement.play()
   }
 }
