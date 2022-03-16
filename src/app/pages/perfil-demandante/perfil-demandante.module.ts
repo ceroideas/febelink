1ef60a0883090f39f0a0c23fbba8b3796324831b
abdias.dev <abdias.dev@gmail.com>
2022-03-16 16:04:29 +0100

    Added Follow Button Component to Public Perfil

--- src/app/pages/perfil-demandante/perfil-demandante.module.ts
+++ src/app/pages/perfil-demandante/perfil-demandante.module.ts
@@ -1,4 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FollowerButtonModule } from 'src/app/components/follower/follower.module';
 import { SharedModule } from '../../shared/shared.module';
 import { SharedPostModule } from '../posts/shared-post.module';
 
@@ -11,6 +12,7 @@ import { PerfilDemandantePage } from './perfil-demandante.page';
       SharedModule
     , PerfilDemandantePageRoutingModule
     , SharedPostModule
+    , FollowerButtonModule
   ],
   declarations: [PerfilDemandantePage],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
