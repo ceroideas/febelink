3ebc8e1e0d58e83df705d48128853d605ee559ae
abdias.dev <abdias.dev@gmail.com>
2022-03-18 15:57:45 +0100

    Fix image  on Update

--- src/app/pages/posts/advises/advise-crud/advise-crud.page.ts
+++ src/app/pages/posts/advises/advise-crud/advise-crud.page.ts
@@ -116,8 +116,6 @@ export class AdviseCRUDPage implements OnInit
     this.content.html = this.iAdvise?.content
     this.iFile.src = this.iAdvise?.media_url
     this.iFile.ext = this.iAdvise?.media_ext
-
-    console.log({ iFile: this.iFile, iAdvise: this.iAdvise })
   }
 
   clear()
@@ -132,6 +130,11 @@ export class AdviseCRUDPage implements OnInit
   fileSelected( file: IFile )
   {
     this.iFile = file
+    if( file == null && this.iAdvise )
+    {
+      this.iAdvise.media_name = null
+      this.iAdvise.media_ext = null
+    }
   }
 
   wysiwygChange( content: iWYSIWYG )
@@ -201,6 +204,8 @@ export class AdviseCRUDPage implements OnInit
       , content: this.content.html
       
       , media: this.iFile?.file
+      , media_name: this.iAdvise?.media_name
+      , media_ext: this.iAdvise?.media_ext
     }
 
     const { response, error } = !this.id
