102a5cb75fd599099719695beea0caa5ef65b9f1
Adolfo Ruiz <ruizruiz.adolfo@gmail.com>
2022-09-06 09:42:37 +0200

    Maquetación desktop y corrección errores mobile

--- src/app/pages/posts/advises/advise-crud/advise-crud.page.ts
+++ src/app/pages/posts/advises/advise-crud/advise-crud.page.ts
@@ -242,6 +242,7 @@ export class AdviseCRUDPage implements OnInit {
         backdropDismiss: false,
       })
     ) {
+      this.adviseSvc.showCreatePost = !this.adviseSvc.showCreatePost;
       this.shareAdvise();
     }
   }
