7bc6cad443586d6e88dec646cdc4e8629382950f
David Bernal <david.bernal@febelink.com>
2023-02-24 12:10:24 +0100

    Adapted naming

---
+++ src/app/pages/cuenta-profesional/Services/cuenta-profesional.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpService, IHttpService } from 'src/app/services/http.service';
+import { ProfessionType } from '../cuenta-profesional.page';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CuentaProfesionalService {
+  constructor(private http: HttpService) {}
+
+  async getMyProfessions() {
+    return this.http.get('user/profession');
+  }
+
+  async getProfessionsByFilter(filterTerm: string) {
+    return this.http.post('collections/profession/filter', { filterTerm });
+  }
+
+  async updateProfessions(professions: ProfessionType[]) {
+    return this.http.put('user/profession', {
+      professions: JSON.stringify(professions),
+    });
+  }
+}
