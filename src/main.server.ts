632f1ba2ec872ec90a5422e45c64e5bfab78047d
Robert Cojocaru <robert@randrei.com>
2024-11-28 10:48:23 +0100

    fix: angular running in development mode

--- src/main.server.ts
+++ src/main.server.ts
@@ -3,9 +3,12 @@ import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { getDocument, getWindow } from "ssr-window";
 import 'localstorage-polyfill';
+import { enableProdMode } from '@angular/core';
 
 global['localStorage'] = localStorage;
 global['document'] = getDocument();
+
+enableProdMode();
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
