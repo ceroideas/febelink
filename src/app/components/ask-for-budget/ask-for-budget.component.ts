dcdcb44e237249f43d3db29f7371c2448fb75696
jorgeceroideas <jorge@ceroideas.es>
2025-06-16 11:08:39 -0400

    changes

--- src/app/components/ask-for-budget/ask-for-budget.component.ts
+++ src/app/components/ask-for-budget/ask-for-budget.component.ts
@@ -61,6 +61,8 @@ export class AskForBudgetComponent implements OnInit {
   async ngOnInit() {
     this.currentUser = {...(await this.utilities.getUserData())};
 
+    console.log(this.currentUser);
+
     this.keywordService.getSubSectorAll()
     .then((data: any) => {
       this.subsectors = data.response;
